240cf2ba0c6b1778bf695ae7ca0c43d628848ace
José Soares <josepsoares@ua.pt>
2023-01-17 00:10:29 +0000

    add feature to order posts by their date (recent or older)

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # bobo's blog
 
-![Yarn](https://img.shields.io/badge/Yarn-2C8EBB.svg?style=for-the-badge&logo=Yarn&logoColor=white) ![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white) ![Astro](https://img.shields.io/badge/Astro-FF5D01.svg?style=for-the-badge&logo=Astro&logoColor=white) ![Svelte](https://img.shields.io/badge/Svelte-FF3E00.svg?style=for-the-badge&logo=Svelte&logoColor=white) ![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white) ![Markdown](https://img.shields.io/badge/markdown-%23000000.svg?style=for-the-badge&logo=markdown&logoColor=white) ![Prettier](https://img.shields.io/badge/Prettier-F7B93E.svg?style=for-the-badge&logo=Prettier&logoColor=black)
+![Yarn](https://img.shields.io/badge/Yarn-2C8EBB.svg?style=for-the-badge&logo=Yarn&logoColor=white) ![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white) ![Astro](https://img.shields.io/badge/Astro-FF5D01.svg?style=for-the-badge&logo=Astro&logoColor=white) ![Vite](https://img.shields.io/badge/Vite-646CFF.svg?style=for-the-badge&logo=Vite&logoColor=white) ![Svelte](https://img.shields.io/badge/Svelte-FF3E00.svg?style=for-the-badge&logo=Svelte&logoColor=white) ![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white) ![Iconify](https://img.shields.io/badge/Iconify-1769AA.svg?style=for-the-badge&logo=Iconify&logoColor=white) ![Markdown](https://img.shields.io/badge/markdown-%23000000.svg?style=for-the-badge&logo=markdown&logoColor=white) ![Prettier](https://img.shields.io/badge/Prettier-F7B93E.svg?style=for-the-badge&logo=Prettier&logoColor=black)
 
 ## about
 
 this is the repo of a simple blog built with astro for the fictional character bobo write some silly stuff on the web. ✍️
 you can also check the other bobo website - [bobo's zoomeranime](). ⛩️
+the language of the blog is portuguese (PT-PT)
 yup, that's it. 👋
 
 ## requirements
--- src/components/PostsList.svelte
+++ src/components/PostsList.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
   import type { Action } from "svelte/action";
+  import { onMount } from "svelte";
+  import { scale } from "svelte/transition";
   import autoAnimate from "@formkit/auto-animate";
+  import dayjs from "dayjs";
+  import customParseFormat from "dayjs/plugin/customParseFormat";
 
   import PostCard from "./PostCard.svelte";
+  import CheckIcon from "../icons/check.svg?component";
+  import CaretDownIcon from "../icons/caret-down.svg?component";
+
+  // set customParseFormat plugin
+  dayjs.extend(customParseFormat);
 
   type PostArr = {
     id: string;
@@ -21,12 +30,13 @@
 
   export let allPosts: PostArr;
 
+  let show = false; // menu state
+  let menu: HTMLElement | null = null; // menu wrapper DOM reference
+  let order = 0; // order state, 0 desc, 1 asc
+
   let page = 0;
   let postsPerPage = 6;
   let posts = allPosts.slice(0, postsPerPage);
-  let newPostsBatch: PostArr = [];
-
-  $: posts = [...posts, ...newPostsBatch];
 
   // https://github.com/formkit/auto-animate/issues/82
   const typedAutoAnimate = autoAnimate as Action;
@@ -35,13 +45,129 @@
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
       if (posts.length !== allPosts.length) {
         page++;
-        newPostsBatch = allPosts.slice(posts.length, postsPerPage * page);
+        posts.push(...allPosts.slice(posts.length, postsPerPage * page));
+
+        if (order === 0) {
+          posts.sort((a, b) =>
+            dayjs(a.data.date, "DD/MM/YYYY").isBefore(
+              dayjs(b.data.date, "DD/MM/YYYY")
+            )
+              ? 1
+              : -1
+          );
+        } else {
+          posts.sort((a, b) =>
+            dayjs(a.data.date, "DD/MM/YYYY").isAfter(
+              dayjs(b.data.date, "DD/MM/YYYY")
+            )
+              ? 1
+              : -1
+          );
+        }
+
+        posts = posts;
       }
     }
   }
+
+  onMount(() => {
+    const handleOutsideClick = (event: Event) => {
+      if (show && !menu?.contains(event.target as Node)) {
+        show = false;
+      }
+    };
+
+    const handleEscape = (event: KeyboardEvent) => {
+      if (show && event.key === "Escape") {
+        show = false;
+      }
+    };
+
+    // add events when element is added to the DOM
+    document.addEventListener("click", handleOutsideClick, false);
+    document.addEventListener("keyup", handleEscape, false);
+
+    // remove events when element is removed from the DOM
+    return () => {
+      document.removeEventListener("click", handleOutsideClick, false);
+      document.removeEventListener("keyup", handleEscape, false);
+    };
+  });
 </script>
 
 <svelte:window on:scroll={scrollHandler} />
+<div class="relative w-min ml-auto" bind:this={menu}>
+  <div class="flex justify-end">
+    <button
+      on:click={() => (show = !show)}
+      class="flex flex-row items-center border-b-2 border-secondary p-2 px-4 transition-all duration-500 ease-in-out hover:text-primary-600 active:text-primary-500"
+    >
+      <span>ordenar</span>
+      <CaretDownIcon class="inline w-4 h-4 ml-2" />
+    </button>
+    {#if show}
+      <div
+        in:scale={{ duration: 100, start: 0.95 }}
+        out:scale={{ duration: 75, start: 0.95 }}
+        class="z-10 origin-top-right absolute right-0 top-10 w-56 py-2 mt-1 bg-primary-800
+          rounded shadow-md"
+      >
+        <button
+          class={`px-2 w-full flex flex-row items-center active:bg-primary-600 hover:bg-primary-600 ${
+            order === 0 ? "bg-primary-700" : "bg-primary-800"
+          }`}
+          on:click={() => {
+            if (order !== 0) {
+              order = 0;
+
+              posts.sort((a, b) =>
+                dayjs(a.data.date, "DD/MM/YYYY").isBefore(
+                  dayjs(b.data.date, "DD/MM/YYYY")
+                )
+                  ? 1
+                  : -1
+              );
+              posts = posts;
+            }
+
+            show = false;
+          }}
+        >
+          <CheckIcon
+            class={order === 0 ? `opacity-100 mr-2` : "mr-2 opacity-0"}
+          />
+          <span class="ml-1">Data (recente-antigo)</span>
+        </button>
+        <button
+          class={`px-2 w-full flex flex-row items-center active:bg-primary-600 hover:bg-primary-600 ${
+            order === 1 ? "bg-primary-700" : "bg-primary-800"
+          }`}
+          on:click={() => {
+            if (order !== 1) {
+              order = 1;
+
+              posts.sort((a, b) =>
+                dayjs(a.data.date, "DD/MM/YYYY").isAfter(
+                  dayjs(b.data.date, "DD/MM/YYYY")
+                )
+                  ? 1
+                  : -1
+              );
+              posts = posts;
+            }
+
+            show = false;
+          }}
+        >
+          <CheckIcon
+            class={order === 1 ? `opacity-100 mr-2` : "mr-2 opacity-0"}
+          />
+          <span class="ml-1">Data (antigo-recente)</span>
+        </button>
+      </div>
+    {/if}
+  </div>
+</div>
 <ul
   use:typedAutoAnimate={{ duration: 500 }}
   class="grid grid-cols-1 gap-4 py-6"
