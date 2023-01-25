3ad01b2797f45b292ba7eb5cd094e8ef873f5717
José Soares <josepsoares@ua.pt>
2023-01-25 03:40:48 +0000

    update to astro v2 and change client:only to client:load

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 import { z, defineCollection } from "astro:content";
 
 const blog = defineCollection({
-  schema: {
+  schema: z.object({
     title: z.string(),
     date: z.string(),
     draft: z.boolean(),
     bannerImg: z.boolean().optional(),
     cardImg: z.boolean().optional(),
     subjects: z.array(z.string()),
-  },
+  }),
 });
 
 export const collections = {
